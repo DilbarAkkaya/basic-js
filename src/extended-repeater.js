4768cde83cb772b98fb3d6ff473eb3ed86d4e155
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-02-23 01:40:13 +0300

    feat: implement task er

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+//const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  if (options.hasOwnProperty('repeatTimes') && options.repeatTimes === undefined) {
+    options.repeatTimes = 1
+  } 
+  if (options.hasOwnProperty('additionRepeatTimes') && options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1
+  }
+  if (options.hasOwnProperty('addition') && options.addition === undefined) {
+    options.addition = ''
+  }
+  if (!options.hasOwnProperty('additionSeparator') && options.additionSeparator === undefined) {
+    options.additionSeparator = '|'
+  }
+  if (!options.hasOwnProperty('separator')) {
+    options.separator = '+'
+  }
+  if (!options.hasOwnProperty('additionRepeatTimes') && options.repeatTimes) {
+    return (str + (options.separator + str).repeat(options.repeatTimes - 1))
+  }
+  if (!options.hasOwnProperty('additionRepeatTimes') && !options.hasOwnProperty('options.repeatTimes')) {
+    return (str + options.addition);
+  }
+  return (str + ((options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1)) + options.addition) + (options.separator + (str + ((options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1)) + options.addition)).repeat(options.repeatTimes - 1);
 };
-  
