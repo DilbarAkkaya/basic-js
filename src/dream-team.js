86245f9be91cf4ad9e2cd5a5788c51bea0978453
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-02-24 13:48:19 +0300

    feat: implement task dt

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  //members = ['Ivan', 'Marya', null, 242, 'Greg Mac']
+  if (Array.isArray(members)){
+  let nameOfCommand = ''; //'GIM'
+  let elementMembersSplit;
+  let firstChatArray = [];
+  for (let i = 0; i < members.length; i++) {
+    if(typeof members[i] === 'string') {
+      elementMembersSplit = members[i].trim().split('');// [I v a n, M a r y a, G r e g M a c]
+      firstChatArray.push(elementMembersSplit[0].toUpperCase());// [I, M, G]
+    }  
+  }
+    return nameOfCommand = firstChatArray.sort().join('') //[GIM]
+} else {
+  return false;
+}
+}
